695985ac48a88894ffe25a03c5fd3d086ce0e5ac
DimitarGP <petkov.dg@gmail.com>
2017-11-04 11:47:51 +0200

    Added solution of Problem 1 from lab: Create and test class BankAccount

---
+++ DefiningClasses/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿namespace DefiningClasses
+{
+    class BankAccount
+    {
+        public BankAccount()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+
+        public void Withdrow(decimal amount)
+        {
+            this.Balance -= amount;
+        }
+    }
+}
---
+++ DefiningClasses/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.InteropServices;
+
+namespace DefiningClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, BankAccount> accounts = new Dictionary<int, BankAccount>();
+
+            string command; 
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var cmdArgs = command.Split();
+                var cmdType = cmdArgs[0];
+
+                switch (cmdType)
+                {
+                    case "Create":
+                        Create(cmdArgs, accounts);
+                        break;
+                    case "Deposit":
+                        Deposit(cmdArgs, accounts);
+                        break;
+                    case "Withdrow":
+                        Withdrow(cmdArgs, accounts);
+                        break;
+                    case "Print":
+                        Print(cmdArgs, accounts);
+                        break;
+                }
+            }
+        }
+
+        private static void Print(string[] cmdArgs, Dictionary<int, BankAccount> accounts)
+        {
+            int id = int.Parse(cmdArgs[1]);
+            var acc = accounts[id];
+            Console.WriteLine($"Account ID {acc.Id}, balance = {acc.Balance}");
+        }
+
+        private static void Withdrow(string[] cmdArgs, Dictionary<int, BankAccount> accounts)
+        {
+            int id = int.Parse(cmdArgs[1]);
+            decimal amount = decimal.Parse(cmdArgs[2]);
+            var acc = accounts[id];
+            acc.Withdrow(amount);
+
+        }
+
+        private static void Deposit(string[] cmdArgs, Dictionary<int, BankAccount> accounts)
+        {
+            int id = int.Parse(cmdArgs[1]);
+            decimal amount = decimal.Parse(cmdArgs[2]);
+            var acc = accounts[id];
+            acc.Deposit(amount);
+        }
+
+        private static void Create(string[] cmdArgs, Dictionary<int, BankAccount> accounts)
+        {
+            int id = int.Parse(cmdArgs[1]);
+
+            if (accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Account already exists");
+            }
+
+            else
+            {
+                BankAccount acc = new BankAccount();
+                acc.Id = id;
+                accounts.Add(id, acc);
+            }
+        }
+    }
+}
